1f9c619451dfbf9573363e17afba4226d5e50d1b
Alexey Ahoshkov <alex.agoshkov6@gmail.com>
2020-07-21 18:49:40 +0300

    Updatd_Chat
    Updatd_Chat

---
+++ MySocNet/Services/Interfaces/IImageService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using MySocNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySocNet.Services.Interfaces
+{
+    public interface IImageService
+    {
+        Task<string> UploadImageAsync(string path);
+        Task<Image> AddImageAsync(IFormFile input);
+        Task<byte[]> GetImageByBytesAsync(string path);
+    }
+}
